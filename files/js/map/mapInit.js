d72a85d4b7683c58b3cd2b472da17d35e8df514c
Florent <florentlepro@hotmail.fr>
2015-03-24 11:41:30 +0100

    Center of the Map

--- files/js/map/mapInit.js
+++ files/js/map/mapInit.js
@@ -2,8 +2,15 @@ function GetMap()
 {
 	var mapOptions = {
 						credentials: "AsA8oS2mP9AjL-xXtE6TK_oDzrrzZV9_5IB4-8cWYfis6CrFTCwukZia0lT-3CZ0",
-						mapTypeId: Microsoft.Maps.MapTypeId.aerial
-					 }
+						center: new Microsoft.Maps.Location(50.69752, 2.86048),
+                        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+                        zoom: 16
+                     }
 
 	var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+	/* TO SET BOUNDARIES ON VIEWING THE MAP */
+	
+	//var viewBoundaries = Microsoft.Maps.LocationRect.fromLocations(new Microsoft.Maps.Location(47.618594, -122.347618), new Microsoft.Maps.Location(47.620700, -122.347584), new Microsoft.Maps.Location(47.622052, -122.345869));
+    //map.setView({ bounds: viewBoundaries});
 }
