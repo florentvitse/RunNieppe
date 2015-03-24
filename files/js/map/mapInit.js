df99e2ddb125708d5388e4f6f1e926022504c923
Florent <florentlepro@hotmail.fr>
2015-03-24 15:23:31 +0100

    Add a addPushpin method

--- files/js/map/mapInit.js
+++ files/js/map/mapInit.js
@@ -1,4 +1,4 @@
-function GetMap()
+function getMap()
 {
 	var mapOptions = {
 						credentials: "AsA8oS2mP9AjL-xXtE6TK_oDzrrzZV9_5IB4-8cWYfis6CrFTCwukZia0lT-3CZ0",
@@ -9,8 +9,20 @@ function GetMap()
 
 	var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
 
+	return map;
+
 	/* TO SET BOUNDARIES ON VIEWING THE MAP */
 	
 	//var viewBoundaries = Microsoft.Maps.LocationRect.fromLocations(new Microsoft.Maps.Location(47.618594, -122.347618), new Microsoft.Maps.Location(47.620700, -122.347584), new Microsoft.Maps.Location(47.622052, -122.345869));
     //map.setView({ bounds: viewBoundaries});
+}
+
+function addPushpin(map, param)
+{
+	// Add a pin to the map
+    var pin = new Microsoft.Maps.Pushpin(param); 
+    map.entities.push(pin);
+
+    // Center the map on the location
+    map.setView({center: param});
 }
