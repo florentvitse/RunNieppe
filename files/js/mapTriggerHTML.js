9ffc4088f830e66744c339f186b10038afe3c561
Florent <florentlepro@hotmail.fr>
2015-04-08 15:47:41 +0200

    Loop remodeled

--- files/js/mapTriggerHTML.js
+++ files/js/mapTriggerHTML.js
@@ -20,12 +20,14 @@ function updateProgressBar()
     var arrayDistance = [5000, 10000, 15000, 21000, 30000, 42195];
     var distTest = 0;
        
-    do {
+    while((distTest < arrayDistance.length - 3) && (totalDistance > arrayDistance[distTest + 1]))
+    {
         distTest++;
-    } while((distTest < arrayDistance.length - 2) && (totalDistance > arrayDistance[distTest]));
+    }
+
 
     // Start in the array of distance
-    alert(distTest - 1);
+    alert(distTest);
     //alert($(labelBar[key]).text());
 
     for (key in arrayBar) {
